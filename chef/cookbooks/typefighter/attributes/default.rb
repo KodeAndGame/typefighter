5b9c3509f05bceeaeb305191359f514d95b53e9b
Richard Alvarez <alvarez.richard@gmail.com>
2015-05-30 17:09:31 -0400

    recipe now installs nodejs v0.12.4

---
+++ chef/cookbooks/typefighter/attributes/default.rb
@@ -0,0 +1,3 @@
+node.set['nodejs']['version'] = '0.12.4'
+node.set['nodejs']['install_method'] = 'source'
+node.set['nodejs']['source']['checksum'] = '3298d0997613a04ac64343e8316da134d04588132554ae402eb344e3369ec912'
