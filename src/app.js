a543ca95c86bb79dc21e0d3f6e24687dfd16db27
unknown <RAlvarez@US-LT365.global.local>
2015-06-03 15:36:06 -0400

    Added typings for VSCode intellisense

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
+/// <reference path="../typings/node/node.d.ts"/>
 var wordChooser = require('./lib/random-word-chooser');
 var path = require('path');
 var express = require('express');
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join( __dirname, 'public')));
 
 app.get('/randomWord', function(req, res) {
-	res.send(wordChooser.get());
+	res.send(wordChooser.get()); 
 });
 
 app.get('/allWords', function(req, res) {
