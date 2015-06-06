b557bb9bdfe753429ed4e8bc3be9789464bd7f21
Richard Alvarez <alvarez.richard@gmail.com>
2015-06-05 23:44:30 -0400

    WIP express handlebars

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 /// <reference path="../typings/node/node.d.ts"/>
-var words = require('./lib/words');
-var path = require('path');
-var express = require('express');
-var app = express();
+var words = require('./lib/words'),
+	passage = require('./lib/passage'),
+	path = require('path'),
+	express = require('express'),
+	exphbs = require('express-handlebars'),
+	app = express();
 
+
+//View Engine
+app.engine('handlebars', exphbs({
+	defaultLayout : 'main',
+	extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+//Static
 app.use(express.static(path.join( __dirname, 'public')));
 
+//Test "API" Calls
 app.get('/randomWord', function(req, res) {
 	res.send(words.get()); 
 });
@@ -14,6 +26,12 @@ app.get('/allWords', function(req, res) {
 	res.send(words.showAll());
 });
 
+app.get('/passage', function (req, res) {
+	res.send(passage.get());
+});
+
+
+// Go
 var server = app.listen(8080, function () {
 
   var host = server.address().address;
